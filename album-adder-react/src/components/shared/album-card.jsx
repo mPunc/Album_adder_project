be27c4006e7da22a02296a5024534b5163b4bdd7
Marko Punčec <mpuncec46@gmail.com>
2025-04-01 13:35:17 +0200

    Some visuals (help)
    -MP

--- album-adder-react/src/components/shared/album-card.jsx
+++ album-adder-react/src/components/shared/album-card.jsx
@@ -18,7 +18,7 @@ function AlbumCards() {
     }, []); // Empty dependency array -> Runs only once when the component mounts
 
     return (
-        <div>
+        <div className='album-card-container-main'>
             {albums.length > 0 ? (
                 albums.map((album, index) => <AlbumCard key={index} oneAlbumDataXml={album} />)
             ) : (
@@ -28,16 +28,22 @@ function AlbumCards() {
     );
 }
 
-function AlbumCard({ oneAlbumDataXml }) { 
+export function AlbumCard({ oneAlbumDataXml }) {
     try {
+        let producersArray = Array.from(oneAlbumDataXml.getElementsByTagName("producenti")[0].getElementsByTagName("producent"));
+        let producers = "";
+        producersArray.forEach(prod => producers += prod.childNodes[0].nodeValue + ", ")
+        producers = producers.replace(/,\s*$/, ""); //This regular expression will remove the last comma and any whitespace after it
+        
+
         return (
-            <div>
-                <h2>{oneAlbumDataXml.getElementsByTagName("naslov")[0]?.innerHTML || "N/A"}</h2>
-                <div>ID: {oneAlbumDataXml.getElementsByTagName("id")[0]?.innerHTML || "N/A"}</div>
-                <div>Umjetnik: {oneAlbumDataXml.getElementsByTagName("umjetnik")[0]?.innerHTML || "N/A"}</div>
-                <div>Žanr: {oneAlbumDataXml.getElementsByTagName("zanr")[0]?.innerHTML || "N/A"}</div>
-                <div>Godina: {oneAlbumDataXml.getElementsByTagName("godina")[0]?.innerHTML || "N/A"}</div>
-                <div>Producenti: {oneAlbumDataXml.getElementsByTagName("producenti")[0]?.innerHTML || "N/A"}</div>
+            <div className='album-card-main'>
+                <h2 className='card-title'>{oneAlbumDataXml.getElementsByTagName("naslov")[0]?.childNodes[0].nodeValue || "N/A"}</h2>
+                <div>ID: {oneAlbumDataXml.getElementsByTagName("id")[0]?.childNodes[0].nodeValue || "N/A"}</div>
+                <div>Artist: {oneAlbumDataXml.getElementsByTagName("umjetnik")[0]?.childNodes[0].nodeValue || "N/A"}</div>
+                <div>Genre: {oneAlbumDataXml.getElementsByTagName("zanr")[0]?.childNodes[0].nodeValue || "N/A"}</div>
+                <div>Year: {oneAlbumDataXml.getElementsByTagName("godina")[0]?.childNodes[0].nodeValue || "N/A"}</div>
+                <div>Producers: {producers || "N/A"}</div>
             </div>
         );
     } catch (error) {
