fac00659f65e642515083b12bfa28f5948ad07bf
Marko Punčec <mpuncec46@gmail.com>
2025-04-03 14:54:31 +0200

    komentari u album-card
    -MP

--- album-adder-react/src/components/shared/album-card.jsx
+++ album-adder-react/src/components/shared/album-card.jsx
@@ -17,6 +17,20 @@ function AlbumCards() {
             .catch(error => console.error("Error loading XML:", error));
     }, []); // Empty dependency array -> Runs only once when the component mounts
 
+    /* ovaj map je malo čudni, na znam ak je ovaj key value ok za posle
+        mislim da bi najbolje  bilo, imati posebni js file koji preuzima xml, obradi podatke kak treba i pohrani ga u useState
+        onda bi lakše bilo mapirati podatke kam treba, ili barem kaj vrati id nekak, jer ipak je zadatak delati u xml-u,
+        a ovo bolje dela s .json files
+        3. 4. 2025. nakon https://youtu.be/tHjxSVaj_wY
+
+        15 min later
+        mislim da bi mogel napraviti funkciju koja vraća id albuma i album ko dva elementa objekta
+        malo jeje sjebano, ali možda bi radilo
+
+        na listi albuma, delete i hide opcije za gumbe, hide bu samo maknul z liste trenutno, a vrne se s refreshom,
+        a delete ga baš reši
+    */
+
     return (
         <div className='album-card-container-main'>
             {albums.length > 0 ? (
